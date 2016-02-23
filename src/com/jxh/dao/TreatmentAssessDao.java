b75b8023b74efa398434cdbc6da1f03ffd085457
hwh <hwh@163.com>
2016-02-23 18:05:09 +0800

    5

---
+++ src/com/jxh/dao/TreatmentAssessDao.java
@@ -0,0 +1,39 @@
+package com.jxh.dao;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.List;
+
+import com.fg.daoImpl.DaoImpl;
+import com.fg.utils.PageUtils;
+import com.fg.utils.ToolsUtils;
+import com.jxh.vo.Treatment;
+import com.jxh.vo.TreatmentAssess;
+
+public class TreatmentAssessDao extends DaoImpl {
+	
+	@Override
+	protected String getSqlPropertiesPath() {
+		return "/sqls/Macao_JXH/treatmentAssess.properties";
+	}
+	
+	public int insertTreatmentAssess(TreatmentAssess treatmentAssess ) throws SQLException, IOException, NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException, ParseException{
+		String sql = this.getSqlByPropKey(ToolsUtils.getCurrentMethodName());
+		Object[] params = getInsertParams(sql, treatmentAssess);
+		return this.update(sql, params);
+	}
+	
+	public int updateTreatmentAssess(TreatmentAssess treatmentAssess) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException, SQLException, ParseException, IOException{
+		String sql = this.getSqlByPropKey(ToolsUtils.getCurrentMethodName());
+		Object[] params = getUpdateParams(sql, treatmentAssess);
+		
+		return this.update(sql, params);
+	}
+
+	public int deleteTreatmentAssessByTreatmentID(String treatmentID) throws SQLException{
+		String sql = "delete from Treatment where treatmentID = ? ";
+		return this.update(sql, treatmentID);
+	}
+	
+}
